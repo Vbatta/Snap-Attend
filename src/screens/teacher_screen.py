656bf4a51c7f0e1a11aecc4b98f048bb2daef973
Vaibhav Batta <vaibhavbatta777@gmail.com>
2026-09-11 15:49:53 +0530

    Before Attendance sec

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -1,23 +1,140 @@
 import streamlit as st 
+
 from src.ui.base_layout import style_background_dashboard, style_base_layout 
+
 from src.components.header import header_dashboard 
 from src.components.footer import footer_dashboard
+
+from src.database.db import check_teacher_exists,create_teacher,teacher_login,get_teacher_subject
+from src.components.dialog_create_subject import create_subject_dialog
+from src.components.dialog_share_subject import share_subject_dialog
+from src.components.subject_card import subject_card
 
 def teacher_screen(): 
     style_base_layout() 
     style_background_dashboard() 
-   
-    if 'teacher_login_type' not in st.session_state or st.session_state.teacher_login_type=='login':
+    
+    if 'teacher_data' in st.session_state:
+        teacher_dashboard()
+    elif 'teacher_login_type' not in st.session_state or st.session_state.teacher_login_type=='login':
         teacher_screen_login()
     elif st.session_state.teacher_login_type == 'register':
         teacher_screen_register()
+
+
+def login_teacher(username,password):
+    if not username or not password:
+        return False
+    
+    teacher = teacher_login(username,password)
+
+    if teacher:
+        st.session_state.user_role='teacher'
+        st.session_state.teacher_data = teacher
+        st.session_state.is_logged_in = True
+        return True, "Login successful"
+    return False, "Invalid credentials"
+
+def teacher_dashboard():
+    teacher_data = st.session_state.teacher_data
+    st.subheader(f"""Welcome,{teacher_data['name']}""")
+    c1, c2 = st.columns(2, vertical_alignment='center', gap='xxlarge') 
+    with c1: 
+        header_dashboard() 
+    with c2: 
+        if st.button('Logout', type='secondary', key='loginbackbtn1',width='stretch'):
+            st.session_state['is_logged_in'] = False
+            del st.session_state.teacher_data
+            st.rerun()
+
+    st.space()
+
+    if 'current_teacher_tab' not in st.session_state:
+        st.session_state.current_teacher_tab = 'take_attendance'
+
+    tab1,tab2,tab3 = st.columns(3)
+
+    with tab1:
+        type1 = 'primary' if st.session_state.current_teacher_tab == 'take_attendance' else 'tertiary'
+        if st.button('Take Attendance',type = type1,width ='stretch',icon=':material/ar_on_you:'):
+            st.session_state.current_teacher_tab ='take_attendance'
+            st.rerun()
+
+    with tab2:
+        type2 = 'primary' if st.session_state.current_teacher_tab == 'manage_subjects' else 'tertiary'
+        if st.button('Manage Subjects',type = type2,width ='stretch',icon=':material/book_ribbon:'):
+            st.session_state.current_teacher_tab ='manage_subjects'
+            st.rerun()
+
+    with tab3:
+        type3 = 'primary' if st.session_state.current_teacher_tab == 'attendance_records' else 'tertiary'
+        if st.button('Attendance Records',type = type3 ,width ='stretch',icon=':material/cards_stack:'):
+            st.session_state.current_teacher_tab ='attendance_records'
+            st.rerun()
+
+
+    st.divider()
+
+    if st.session_state.current_teacher_tab == 'take_attendance':
+        teacher_tab_take_attendance()
+    if st.session_state.current_teacher_tab == 'manage_subjects':
+        teacher_tab_manage_subjects()
+    if st.session_state.current_teacher_tab == 'attendance_records':
+        teacher_tab_attendance_records()
+
+    footer_dashboard()
+
+    
+
+def teacher_tab_take_attendance():
+    st.header('Take AI Attendance')
+
+def teacher_tab_manage_subjects():
+    teacher_id = st.session_state.teacher_data['teacher_id']
+    col1,col2 =st.columns(2)
+    with col1:
+        st.header('Manage Subjects',width='stretch')
+    with col2:
+        if st.button('Create New Subject',width='stretch'):
+            create_subject_dialog(teacher_id)
+
+        teacher_id=st.session_state.teacher_data['teacher_id']
+        subjects = get_teacher_subject(teacher_id)
+    if subjects:
+        for sub in subjects:
+            stats =[
+                ("🫂",'Students',sub['total_students']),
+                ("🕰️",'Classes',sub['total_classes']),
+
+            ]
+
+            def share_btn():
+                if st.button(f"Share Code: {sub['name']}",key=f"share_{sub['subject_code']}",icon=':material/share:'):
+                    share_subject_dialog(sub['name'],sub['subject_code'])
+                st.space()
+
+            subject_card(
+                name= sub['name'],
+                code = sub['subject_code'],
+                section = sub['section'],
+                stats= stats,
+                footer_callback = share_btn
+
+            )
+    else:
+        st.info('No subjects Found. Create one above.')
+
+def teacher_tab_attendance_records():
+    st.header('Attendance Records')
+
+
  
 def teacher_screen_login(): 
     c1, c2 = st.columns(2, vertical_alignment='center', gap='xxlarge') 
     with c1: 
         header_dashboard() 
     with c2: 
-        if st.button('Go back to Home', type='secondary', key='loginbackbtn1'):
+        if st.button('Go back to Home', type='secondary', key='loginbackbtn1',width='stretch'):
             st.session_state['login_type'] = None
             st.rerun()
      
@@ -30,22 +147,47 @@ def teacher_screen_login():
 
     btnc1, btnc2 = st.columns(2)
 
+    footer_dashboard()
+
     with btnc1:
-        st.button('Login',icon=':material/passkey:',shortcut='control+enter',width='stretch')
+        if st.button('Login',icon=':material/passkey:',shortcut='control+enter',width='stretch'):
+
+            if login_teacher(teacher_username,teacher_pass):
+                st.toast('Welcome back!',icon='👋')
+                import time
+                time.sleep(1)
+                st.rerun()
+            else:
+                st.error('Invlaid username and password combo')
 
     with btnc2:
         if st.button('Register Instead',icon=':material/passkey:',type='primary',width='stretch'):
             st.session_state.teacher_login_type='register'
             st.rerun()
 
-    footer_dashboard()
+    
+
+def register_teacher(teacher_username,teacher_name,teacher_pass,teacher_pass_confirm):
+    if not teacher_username or not teacher_name or not teacher_pass:
+        return False,'All Fields are required!'
+    if check_teacher_exists(teacher_username):
+        return False,'Username already taken'
+    if teacher_pass != teacher_pass_confirm:
+        return False,'Password doesn"t match'
+
+    try:
+        create_teacher(teacher_username,teacher_pass,teacher_name)
+        return True, "Registration successful!"
+    except Exception as e:
+        return False,"Unexpected Error!"
+
 
 def teacher_screen_register(): 
     c1, c2 = st.columns(2, vertical_alignment='center', gap='xxlarge') 
     with c1: 
         header_dashboard() 
     with c2: 
-        if st.button('Go back to Home', type='secondary', key='loginbackbtn2'):
+        if st.button('Go back to Home', type='secondary', key='loginbackbtn2',width='stretch'):
             st.session_state['login_type']=None
             st.rerun()
  
@@ -61,11 +203,19 @@ def teacher_screen_register():
     btnc1, btnc2 = st.columns(2)
 
     with btnc1:
-        
-        st.button('Register Now',icon=':material/passkey:',type='primary',width='stretch')
+        if st.button('Register Now',icon=':material/passkey:',type='primary',width='stretch'):
+            success,message = register_teacher(teacher_username,teacher_name,teacher_pass,teacher_pass_confirm)
+            if success:
+                st.success(message)
+                import time
+                time.sleep(2)
+                st.session_state.teacher_login_type ='login'
+                st.rerun()
+            else:
+                st.error(message)
 
     with btnc2:
         if st.button('Login Instead',icon=':material/passkey:',shortcut='control+enter',width='stretch'):
             st.session_state.teacher_login_type='login'
 
-    footer_dashboard()
+  
--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -1,107 +1,116 @@
 import streamlit as st
 
+
+
 def style_background_home():
+
     st.markdown("""
         <style>
-            .stApp {
-                background: #6C79FF !important;
-            }
 
-            .stApp div[data-testid='stColumn']{
-                background-color:#E0E3FF !important;
-                padding: 1.2rem !important;
-                border-radius: 1.5rem !important;
-            }
-        </style>
-       
-    """, unsafe_allow_html=True)
+                .stApp {
+                    background:#7C83FD !important;
+                }
 
+                .stApp div[data-testid="stColumn"]{
+                    background-color:#E0E3FF !important;
+                    padding:2.5rem !important;
+                    border-radius: 5rem !important;
+                    }
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
+    
 
 def style_background_dashboard():
+
     st.markdown("""
         <style>
-            .stApp {
-                background: #ffb6c1 !important;
-            }
-        </style>
-    """, unsafe_allow_html=True)
 
+                .stApp {
+                    background: #D6DBFF !important;
+                }
+
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
+    
+
+    
 
 def style_base_layout():
+# asdasd
     st.markdown("""
         <style>
-        
-        @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&family=DM+Serif+Display:ital@0;1&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Manrope:wght@200..800&family=Outfit:wght@100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;700&family=Inter:wght@300;400;600&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Inter:wght@400;600&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
 
-          # Hide top bar of streamlit 
-          /* #MainMenu, footer, header {
+                
+         /* Hide Top Bar of streamlit */
+                
+         /*   #MainMenu, footer, header {
                 visibility: hidden;
-            */
-
-            .block-container{
-                padding-top:0rem !important;
-                padding-bottom:0rem !important;
-
-            }
-          
-
-            h1{
-                font-family:'Climate Crisis',sans-serif !important;
-                font-size:2.2rem !important;
-                line-height:0.8 !important;
-                margin-bottom:0 rem !important; 
-              # margin-left:7rem !important;
-                font-weight:300px;
-                color:#E0E3FF;
-                text-align:center
+            } */
+                
+            .block-container {
+                padding-top:1.5rem !important;    
             }
 
-            
-            h2{
-                font-family:'Climate Crisis',sans-serif !important;
-                font-size:2rem !important;
-                line-height:1.1 !important;
-                margin-bottom:0 rem !important; 
-                color:#E0E3FF
+            h1 {
+                font-family: 'DM Serif Display', sans-serif !important;
+                font-size: 3.5rem !important;
+                line-height:1.1 1important;
+                margin-bottom:0rem !important;
             }
+                
 
-            h3,h4,p,span{
-                font-family:'Outfit',sans-serif;
-                font-size:3rem
+            h2 {
+                font-family: 'DM Serif Display', sans-serif !important;
+                font-size: 2rem !important;
+                line-height:0.9 !important;
+                margin-bottom:0rem !important;
             }
+                
+            h3, h4, p {
+                font-family: 'Outfit', sans-serif;   
+                font-weight: 500;
+                font-size: 0.95rem !important;
 
-            button[kind='primary']{
-                border-radius:1.5rem !important;
-                background: #6C79FF !important;
-                color:white !important;
-                padding:10px 20px !important
-                border:none !important
-                transition :transform 0.25s ease-in-out !important
             }
+                
 
-            button[kind='secondary']{
-                border-radius:1.5rem !important;
-                background: #EB459E !important;
-                color:white !important;
-                padding:10px 20px !important
-                border:none !important
-                transition :transform 0.25s ease-in-out !important
-            }
+            button{
+                border-radius: 1.5rem !important;
+                background-color: #5865F2 !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+                }
 
-            
-            button[kind='tertiary']{
-                border-radius:1.5rem !important;
-                background: #EB459E !important;
-                color:white !important;
-                padding:10px 20px !important
-                border:none !important
-                transition :transform 0.25s ease-in-out !important
-            }
+            button[kind="secondary"]{
+                border-radius: 1.5rem !important;
+                background-color: #EB459E !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+                }
+
+            button[kind="tertiary"]{
+                border-radius: 1.5rem !important;
+                background-color: black !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+                }
 
             button:hover{
-                transform:scale(1.05)
-            }
-        </style>
-    """, unsafe_allow_html=True)
+                transform :scale(1.05)}
+        </style>  
 
-   
+                """
+            ,unsafe_allow_html=True)
