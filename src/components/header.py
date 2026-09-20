726a2c1b1ed1e1e25b4cc03c42a4b7d920c6d3c1
Vaibhav Batta <vaibhavbatta777@gmail.com>
2026-09-20 13:03:59 +0530

    Changes

--- src/components/header.py
+++ src/components/header.py
@@ -2,18 +2,18 @@ import streamlit as st
 
 def header_home():
 
-    logo_url = "https://raw.githubusercontent.com/Vbatta/DDoS-Attack/refs/heads/main/ChatGPT%20Image%20Sep%2012%2C%202026%2C%2008_38_23%20PM.png"
+    logo_url = "https://raw.githubusercontent.com/Vbatta/Music_Playlist_SQL/refs/heads/main/logo1.png"
 
     st.markdown(f"""
         <div style="display:flex; flex-direction:row; align-items:center; justify-content:center; margin-bottom:20px;margin-top:0px;gap:25px">
-            <img src="{logo_url}" style="height:160px;>" />
+            <img src="{logo_url}" style="height:130px;width:130>" />
             <h1 style="text-align:center; color:#E0E3FF;" >SNAP <br/>ATTEND </h1>
         </div>
     """, unsafe_allow_html=True)
 
 def header_dashboard():
 
-    logo_url = "https://raw.githubusercontent.com/Vbatta/DDoS-Attack/refs/heads/main/ChatGPT%20Image%20Sep%2012%2C%202026%2C%2008_38_23%20PM.png"
+    logo_url = "https://raw.githubusercontent.com/Vbatta/Music_Playlist_SQL/refs/heads/main/logo1.png"
 
     st.markdown(f"""
         <div style="display:flex; align-items:center; justify-content:center;gap:10px; margin-bottom:20px;margin-top:0px">
